83ab3314f413894b45b37b6a656dd0a51a704270
zhazhax <66709349+zhazhax@users.noreply.github.com>
2022-07-28 10:33:33 +0800

    Update hello_triangle_indexed.cpp

--- src/1.getting_started/2.2.hello_triangle_indexed/hello_triangle_indexed.cpp
+++ src/1.getting_started/2.2.hello_triangle_indexed/hello_triangle_indexed.cpp
@@ -10,13 +10,13 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = "#version 330 core\n"      //顶点着色器源码
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource = "#version 330 core\n"      //片段着色器源码
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
